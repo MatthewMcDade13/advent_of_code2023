1c044a5ebdb48cc2e142ea8fe72133030998d20c
MatthewMcDade13 <MatthewMcDade13@tuta.io>
2023-12-23 17:11:21 -0800

    day1 cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,7 +12,7 @@ fn main() -> common::Result<()> {
     let p1_total = part_1(inputs);
     println!("Part 1 Total: {p1_total}");
 
-    let p2_total = part_2(inputs)?;
+    let p2_total = part_2(inputs);
     println!("Part 2 Total: {p2_total}");
 
     Ok(())
@@ -40,13 +40,13 @@ fn part_1(inputs: &[&str]) -> u32 {
     total
 }
 
-fn part_2(inputs: &[&str]) -> common::Result<u32> {
+fn part_2(inputs: &[&str]) -> u32 {
     let total = inputs.iter().fold(0, |acc, line| {
         if line.len() < 1 {
             return acc;
         }
 
-        let nums: Vec<_> = tokenize(line)
+        let nums: Vec<_> = tok::tokenize(line)
             .iter()
             .filter_map(|tok| parse_number(tok).ok())
             .collect();
@@ -59,7 +59,7 @@ fn part_2(inputs: &[&str]) -> common::Result<u32> {
         acc + n
     });
 
-    Ok(total)
+    total
 }
 
 fn parse_number(nstr: &str) -> common::Result<u32> {
@@ -76,130 +76,4 @@ fn parse_number(nstr: &str) -> common::Result<u32> {
         _ => nstr.parse::<u32>()?,
     };
     Ok(n)
-}
-
-fn tokenize(line: &str) -> Vec<&str> {
-    let mut toks = Vec::new();
-
-    let raw = line.as_bytes();
-
-    for i in 0..raw.len() {
-        let c = raw[i] as char;
-
-        match c {
-            'o' => {
-                if let Some(s) = substr(raw, i, tok::ONE.len()) {
-                    toks.push(s);
-                } else {
-                    continue;
-                }
-            }
-            't' => {
-                let peek = if i + 1 >= raw.len() {
-                    continue;
-                } else {
-                    raw[i + 1] as char
-                };
-                match peek {
-                    'w' => {
-                        if let Some(s) = substr(raw, i, tok::TWO.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    'h' => {
-                        if let Some(s) = substr(raw, i, tok::THREE.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    _ => continue,
-                };
-            }
-            'f' => {
-                let peek = if i + 1 >= raw.len() {
-                    continue;
-                } else {
-                    raw[i + 1] as char
-                };
-                match peek {
-                    'o' => {
-                        if let Some(s) = substr(raw, i, tok::FOUR.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    'i' => {
-                        if let Some(s) = substr(raw, i, tok::FIVE.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    _ => continue,
-                }
-            }
-            's' => {
-                let peek = if i + 1 >= raw.len() {
-                    continue;
-                } else {
-                    raw[i + 1] as char
-                };
-                match peek {
-                    'i' => {
-                        if let Some(s) = substr(raw, i, tok::SIX.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    'e' => {
-                        if let Some(s) = substr(raw, i, tok::SEVEN.len()) {
-                            toks.push(s);
-                        } else {
-                            continue;
-                        }
-                    }
-                    _ => continue,
-                }
-            }
-            'e' => {
-                if let Some(s) = substr(raw, i, tok::EIGHT.len()) {
-                    toks.push(s);
-                } else {
-                    continue;
-                }
-            }
-            'n' => {
-                if let Some(s) = substr(raw, i, tok::NINE.len()) {
-                    toks.push(s);
-                } else {
-                    continue;
-                }
-            }
-            '0'..='9' => {
-                if let Some(s) = substr(raw, i, 1) {
-                    toks.push(s);
-                } else {
-                    continue;
-                }
-            }
-            _ => continue,
-        }
-    }
-
-    toks
-}
-
-fn substr(src: &[u8], from: usize, len: usize) -> Option<&str> {
-    let to = from + len;
-    if to > src.len() {
-        None
-    } else {
-        let s = std::str::from_utf8(&src[from..to]).unwrap();
-        Some(s)
-    }
 }
